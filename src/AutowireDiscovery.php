228dbb3b7a50513414e8b405a71a77fc80bcea3b
aidan-casey <aidan@caseyhouse.net>
2024-11-25 12:46:39 +0000

    chore: bump component dependencies (#773)

---
+++ src/AutowireDiscovery.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tempest\Container;
+
+use Tempest\Core\Discovery;
+use Tempest\Core\DiscoveryLocation;
+use Tempest\Core\IsDiscovery;
+use Tempest\Reflection\ClassReflector;
+
+/**
+ * @property GenericContainer $container
+ */
+final class AutowireDiscovery implements Discovery
+{
+    use IsDiscovery;
+
+    public function __construct(
+        private readonly Container $container,
+    ) {
+    }
+
+    public function discover(DiscoveryLocation $location, ClassReflector $class): void
+    {
+        $autowire = $class->getAttribute(Autowire::class);
+
+        if ($autowire === null) {
+            return;
+        }
+
+        $this->discoveryItems->add($location, [$class, $autowire]);
+    }
+
+    public function apply(): void
+    {
+        foreach ($this->discoveryItems as [$class, $autowire]) {
+            if ($autowire !== null) {
+                $this->discoverAsSingleton($class);
+            } else {
+                $this->discoverAsDefinition($class);
+            }
+        }
+    }
+
+    private function discoverAsSingleton(ClassReflector $class): void
+    {
+        $interfaces = $class->getReflection()->getInterfaceNames();
+
+        foreach ($interfaces as $interface) {
+            $this->container->singleton(
+                $interface,
+                static fn (Container $container) => $container->get($class->getName()),
+            );
+        }
+    }
+
+    private function discoverAsDefinition(ClassReflector $class): void
+    {
+        $interfaces = $class->getReflection()->getInterfaceNames();
+
+        foreach ($interfaces as $interface) {
+            $this->container->register(
+                $interface,
+                static fn (Container $container) => $container->get($class->getName()),
+            );
+        }
+    }
+}
